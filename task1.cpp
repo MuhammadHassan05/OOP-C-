cd9597acd8d80fb2c39b5343a5875a220a1ac89a
Muhammad Hassan <156007695+MuhammadHassan05@users.noreply.github.com>
2025-04-26 10:32:44 +0500

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,213 +1,105 @@
 //#include <iostream>
-//#include <cmath>
 //using namespace std;
 //
-//class Fraction {
-//private:
-//    int numerator;
-//    int denominator;
-//
-//    void simplify() {
-//        if (denominator < 0) {
-//            numerator *= -1;
-//            denominator *= -1;
-//        }
-//        int g = gcd(abs(numerator), abs(denominator));
-//        numerator /= g;
-//        denominator /= g;
-//    }
-//
-//    int gcd(int a, int b) const {
-//        return b == 0 ? a : gcd(b, a % b);
-//    }
-//
+//class Animal {
 //public:
-//    // Constructors
-//    Fraction() {
-//        numerator = 0;
-//        denominator = 1;
+//    virtual char* speak() {
+//        char* string = new char[16] {'s', 'p', 'e', 'a', 'k', '(', ')', 'c', 'a', 'l', 'l', 'e', 'd'};
+//        return string;
 //    }
-//
-//    Fraction(int n, int d = 1) {
-//        numerator = n;
-//        denominator = (d == 0 ? 1 : d);
-//        simplify();
-//    }
-//
-//    Fraction(double value) {
-//        const int precision = 1000000;
-//        numerator = static_cast<int>(value * precision);
-//        denominator = precision;
-//        simplify();
-//    }
-//
-//    Fraction(const Fraction& other) {
-//        numerator = other.numerator;
-//        denominator = other.denominator;
-//    }
-//
-//    // Member Functions
-//    double toDecimal() const {
-//        return static_cast<double>(numerator) / denominator;
-//    }
-//
-//    Fraction reciprocal() const {
-//        return Fraction(denominator, numerator);
-//    }
-//
-//    void display() const {
-//        cout << numerator << "/" << denominator << endl;
-//    }
-//
-//    // Arithmetic Operators
-//    Fraction operator+(const Fraction& rhs) const {
-//        Fraction result(numerator * rhs.denominator + rhs.numerator * denominator,
-//            denominator * rhs.denominator);
-//        result.simplify();
-//        return result;
-//    }
-//
-//    Fraction operator-(const Fraction& rhs) const {
-//        Fraction result(numerator * rhs.denominator - rhs.numerator * denominator,
-//            denominator * rhs.denominator);
-//        result.simplify();
-//        return result;
-//    }
-//
-//    Fraction operator*(const Fraction& rhs) const {
-//        Fraction result(numerator * rhs.numerator,
-//            denominator * rhs.denominator);
-//        result.simplify();
-//        return result;
-//    }
-//
-//    Fraction operator/(const Fraction& rhs) const {
-//        Fraction result(numerator * rhs.denominator,
-//            denominator * rhs.numerator);
-//        result.simplify();
-//        return result;
-//    }
-//
-//    // Overload with int
-//    Fraction operator+(int val) const { return *this + Fraction(val); }
-//    Fraction operator-(int val) const { return *this - Fraction(val); }
-//    Fraction operator*(int val) const { return *this * Fraction(val); }
-//    Fraction operator/(int val) const { return *this / Fraction(val); }
-//
-//    // Shorthand Operators
-//    Fraction& operator+=(const Fraction& rhs) {
-//        *this = *this + rhs;
-//        return *this;
-//    }
-//
-//    Fraction& operator-=(const Fraction& rhs) {
-//        *this = *this - rhs;
-//        return *this;
-//    }
-//
-//    Fraction& operator*=(const Fraction& rhs) {
-//        *this = *this * rhs;
-//        return *this;
-//    }
-//
-//    Fraction& operator/=(const Fraction& rhs) {
-//        *this = *this / rhs;
-//        return *this;
-//    }
-//
-//    // Comparison Operators
-//    bool operator==(const Fraction& rhs) const {
-//        return numerator == rhs.numerator && denominator == rhs.denominator;
-//    }
-//
-//    bool operator!=(const Fraction& rhs) const {
-//        return !(*this == rhs);
-//    }
-//
-//    bool operator<(const Fraction& rhs) const {
-//        return numerator * rhs.denominator < rhs.numerator * denominator;
-//    }
-//
-//    bool operator>(const Fraction& rhs) const {
-//        return rhs < *this;
-//    }
-//
-//    bool operator<=(const Fraction& rhs) const {
-//        return !(*this > rhs);
-//    }
-//
-//    bool operator>=(const Fraction& rhs) const {
-//        return !(*this < rhs);
-//    }
-//
-//    // Increment and Decrement
-//    Fraction& operator++() {
-//        numerator += denominator;
-//        simplify();
-//        return *this;
-//    }
-//
-//    Fraction operator++(int) {
-//        Fraction temp = *this;
-//        ++(*this);
-//        return temp;
-//    }
-//
-//    Fraction& operator--() {
-//        numerator -= denominator;
-//        simplify();
-//        return *this;
-//    }
-//
-//    Fraction operator--(int) {
-//        Fraction temp = *this;
-//        --(*this);
-//        return temp;
-//    }
-//
-//    // Function call operator (returns reciprocal)
-//    Fraction operator()() const {
-//        return reciprocal();
-//    }
-//
-//    // Stream operators
-//    friend ostream& operator<<(ostream& out, const Fraction& f) {
-//        out << f.numerator << "/" << f.denominator;
-//        return out;
-//    }
-//
-//    friend istream& operator>>(istream& in, Fraction& f) {
-//        char slash;
-//        in >> f.numerator >> slash >> f.denominator;
-//        f.simplify();
-//        return in;
-//    }
+//    virtual ~Animal() { cout << "~Animal() called." << endl; }
 //};
 //
-//// Main Function to Test All Features
+//class Dog : public Animal {
+//public:
+//    char* speak() override {
+//        char* string = new char[6] {'w', 'o', 'o', 'f', '!'};
+//        return string;
+//    }
+//    ~Dog() { cout << "~Dog() called." << endl; }
+//};
+//
+//class Cat : public Animal {
+//public:
+//    char* speak() override {
+//        char* string = new char[6] {'m', 'e', 'o', 'w', '!'};
+//        return string;
+//    }
+//    ~Cat() { cout << "~Cat() called." << endl; }
+//};
+//
+//class Sheep : public Animal {
+//public:
+//    char* speak() override {
+//        char* string = new char[6] {'b', 'l', 'e', 'e', 't'};
+//        return string;
+//    }
+//    ~Sheep() { cout << "~Sheep() called." << endl; }
+//};
+//
+//class Cow : public Animal {
+//public:
+//    char* speak() override {
+//        char* string = new char[4] {'m', 'o', 'o'};
+//        return string;
+//    }
+//    ~Cow() { cout << "~Cow() called." << endl; }
+//};
+//
+//class Horse : public Animal {
+//public:
+//    char* speak() override {
+//        char* string = new char[6] {'n', 'e', 'i', 'g', 'h'};
+//        return string;
+//    }
+//    ~Horse() { cout << "~Horse() called." << endl; }
+//};
+//
 //int main() {
-//    Fraction f1(3, 4);
-//    Fraction f2(2, 3);
+//    const int size = 5;
+//    Animal* myPets[size];
+//    int i = 0;
 //
-//    cout << "f1 = " << f1 << endl;
-//    cout << "f2 = " << f2 << endl;
+//    while (i < size) {
+//        cout << "Press 1 for a Dog, 2 for a Cat, 3 for a Sheep, 4 for a Cow, and 5 for a Horse." << endl;
+//        int choice;
+//        cin >> choice;
 //
-//    cout << "f1 + f2 = " << f1 + f2 << endl;
-//    cout << "f1 - f2 = " << f1 - f2 << endl;
-//    cout << "f1 * f2 = " << f1 * f2 << endl;
-//    cout << "f1 / f2 = " << f1 / f2 << endl;
+//        switch (choice) {
+//        case 1:
+//            myPets[i] = new Dog;
+//            cout << "Dog added at position " << i << endl << endl;
+//            i++;
+//            break;
+//        case 2:
+//            myPets[i] = new Cat;
+//            cout << "Cat added at position " << i << endl << endl;
+//            i++;
+//            break;
+//        case 3:
+//            myPets[i] = new Sheep;
+//            cout << "Sheep added at position " << i << endl << endl;
+//            i++;
+//            break;
+//        case 4:
+//            myPets[i] = new Cow;
+//            cout << "Cow added at position " << i << endl << endl;
+//            i++;
+//            break;
+//        case 5:
+//            myPets[i] = new Horse;
+//            cout << "Horse added at position " << i << endl << endl;
+//            i++;
+//            break;
+//        default:
+//            cout << "Invalid input. Enter again." << endl << endl;
+//            break;
+//        }
+//    }
 //
-//    cout << "f1.toDecimal() = " << f1.toDecimal() << endl;
-//    cout << "Reciprocal of f1 using function call operator: " << f1() << endl;
-//
-//    cout << "Prefix ++f1 = " << ++f1 << endl;
-//    cout << "Postfix f2++ = " << f2++ << endl;
-//    cout << "After postfix f2 = " << f2 << endl;
-//
-//    Fraction input;
-//    cout << "Enter a fraction (e.g. 5/8): ";
-//    cin >> input;
-//    cout << "You entered: " << input << endl;
-//
+//    for (int j = 0; j < size; j++) {
+//        cout << myPets[j]->speak() << endl;
+//        delete myPets[j];
+//    }
 //    return 0;
 //}
